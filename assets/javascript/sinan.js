0a546a70b119ebcca6621d8cb546b741683aaf4e
sinansamarai <sinan.abdulraheem@gmail.com>
2017-09-24 20:37:26 -0500

    pushing stuff to new branch

--- assets/javascript/sinan.js
+++ assets/javascript/sinan.js
@@ -1,31 +1,154 @@
-// Mapquest API
+ // varibales for map  and markers 
 
-// Firebase INIT
-var config = {
-    apiKey: "AIzaSyDuLnrQSY0AwQnve9SeGKdvajKR1Cf4vT0",
-    authDomain: "mijosi-1505956805893.firebaseapp.com",
-    databaseURL: "https://mijosi-1505956805893.firebaseio.com",
-    projectId: "mijosi-1505956805893",
-    storageBucket: "mijosi-1505956805893.appspot.com",
-    messagingSenderId: "178421641746"
-};
-firebase.initializeApp(config);
+var map;
+var markers = [];
 
-// Database
-var database = firebase.database();
+//  Austin map 
 
-// Variables for grabbing search result info (venue, lat/lng, etc.)
-var venueName = $("#employee-name-input").val().trim();
-var venueLat = $("#role-input").val().trim();
-var venueLng = ($("#start-input").val().trim(), "DD/MM/YY").format("X");
+function initialize() {
+    var austin = new google.maps.LatLng(30.307182,-97.755996 );
 
-// Info for Search Results
-var newResult = {
-    venue: venueName,
-    lat: venueLat,
-    lng: venueLng,
-};
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(30.307182,-97.755996 ),
+        zoom: 10
+    });
 
-// Pushing to Firebase	
-database.ref().push(newResult);
-	console.log(newResult);
+
+
+
+
+    service = new google.maps.places.PlacesService(map);
+}
+
+initialize()
+
+
+// 
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+              createMarker(results[i].geometry.location,);
+            console.log('inside call back ',results);
+        }
+
+    }
+}
+
+
+$("#google-searchButton").on("click", function() {
+    markers.forEach(function(marker) {
+        marker.setMap(null);
+    });
+    var request = {
+        location: new google.maps.LatLng(30.307182,-97.755996 ),
+        radius: '500',
+        query: $("#google-input").val().trim()
+    }
+    service.textSearch(request, callback);
+});
+
+function createMarker(location, data) {
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        mapTypeId: 'terrain'
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content:data
+    });
+  
+    if (data) { 
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+ 
+
+    markers.push(marker);
+}
+
+function getEventsByLocation(lat, lng) {
+    fetch(`https://galvanize-cors-proxy.herokuapp.com/https://api.eventful.com/json/events/search?app_key=FJj2tfhj2XNJM7Jb&location=${lat},${lng}&within=10`)
+        .then(a => a.json())
+        .then(function(response) {
+            response.events.event.forEach(function(event) {
+                console.log('inside request to eventful',event)
+                displayEvent(event.venue_name, event.title)
+
+                createMarker({lat: parseFloat(event.latitude), lng: parseFloat(event.longitude)}, event.description)
+
+            })
+        })
+}
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+    console.log('inside success',crd)
+    getEventsByLocation(crd.latitude, crd.longitude)
+  };
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+
+
+//   function DisplayEvents(response){
+//     response.events.event.forEach(function(event){
+//         var eventDiv = $("<div>");
+//         eventDiv.addClass("ClassEvent");
+//         var eventName = $("<p>");
+//         var eventImage = $("<img>").attr('src', );
+//     })
+
+// }
+
+  //   }
+  // }
+
+
+  function displayEvent (venue, title){
+    console.log('inside displayEvent', venue, title)
+    $("#displayEvents").append('<div id=eventDiv>' +
+     '<p>'+ venue + '</p>' +
+       '<p>'+ title + '</p></div><br/>');
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
